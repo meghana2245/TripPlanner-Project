4b4afc5a536ed67ca7e96ebb7fae4288472d6d97
meghana2245 <meghanakotha245@gmail.com>
2026-05-17 22:16:15 +0530

    Configure CORS with specific origins

--- server/server.js
+++ server/server.js
@@ -15,7 +15,15 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://trip-planner-project-lemon.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
